f7a63ca4dec8bb8ee65b853ba784b030f1376b7d
Sarek <saregoth@yahoo.com>
2020-04-08 00:12:09 +0200

    Added path support

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,14 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+    },
+    {
+      resolve: `gatsby-plugin-root-import`,
+      options: {
+        ["@src"]: `${__dirname}/src`,
+        ["@pages"]: `${__dirname}/src/pages`,
+        ["@components"]: `${__dirname}/src/components`
+      }
     }
   ],
 }
