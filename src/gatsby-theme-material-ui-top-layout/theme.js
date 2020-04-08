95098cc0b830624e1ef54ba48ebf5ed77e64b81b
Sarek <saregoth@yahoo.com>
2020-04-09 00:49:50 +0200

    Set theme settings

--- src/gatsby-theme-material-ui-top-layout/theme.js
+++ src/gatsby-theme-material-ui-top-layout/theme.js
@@ -1,5 +1,19 @@
 import { createMuiTheme } from "@material-ui/core";
+import {lightBlue, teal} from '@material-ui/core/colors';
 
-const theme = createMuiTheme();
+const theme = createMuiTheme({
+    palette: {
+        type: 'dark',
+        primary: teal,
+        secondary: lightBlue
+    },
+    typography: {
+        fontFamily: [
+            'Titillium Web',
+            "Roboto", "Helvetica", "Arial",
+            'sans-serif'
+        ].join(','),
+    },
+});
 
 export default theme;
