b4e99c51d2f4ab45e4cc29d957e1ea314085e967
Sarek <saregoth@yahoo.com>
2020-04-24 14:13:39 +0200

    Updated style

--- src/pages/docs.tsx
+++ src/pages/docs.tsx
@@ -11,20 +11,45 @@ const useStyles = makeStyles(theme => ({
     height: '100%',
     flexDirection: 'column',
     justifyContent: 'flex-start',
-    
   },
   docs: {
-    background: `${Color(theme.palette.secondary.main).darken(0.95).alpha(0.65)}`,
+    background: `${Color(theme.palette.secondary.main).darken(0.95).alpha(0.85)}`,
     borderImage: `url(${svgBorder}) 50 50`,
     boxSizing: 'border-box',
     borderStyle: 'inset',
     borderWidth: 20,
-    color: theme.palette.primary.main,
-    "& > h1, h2,h3, h4, h5, h6": {
-      color: theme.palette.secondary.main,
+    overflowY: "scroll",
+    color: `${Color(theme.palette.secondary.dark).desaturate(0.35)}`,
+    scrollbarWidth: 'thin',
+    scrollbarColor: `${theme.palette.primary.main} ${theme.palette.action.disabledBackground}`,
+    "& > h1, h2,h3, h4, h5, h6, a": {
+      color: theme.palette.secondary.light,
+    },
+    "& a": {
       fontWeight: 'bold',
-      textShadow: `0 0 4px ${Color(theme.palette.secondary.main).alpha(0.65)}`,
-    }
+      textDecoration: 'none',
+      transition: ".8s",
+      "&:hover":{
+        color: theme.palette.common.white
+      }
+    },
+    "& pre": {
+      backgroundColor: `${Color(theme.palette.secondary.dark).desaturate(0.7)}`,
+      width: 'fit-content',
+      padding: theme.spacing(1,2),
+      color: theme.palette.text.primary,
+      borderRadius: theme.shape.borderRadius
+    },
+    "&::-webkit-scrollbar": {
+      width: 6,
+    },
+    "&::-webkit-scrollbar-track": {
+      background: theme.palette.action.disabledBackground,
+    },
+    "&::-webkit-scrollbar-thumb": {
+      backgroundColor: theme.palette.primary.main,
+      border: `3px solid ${theme.palette.action.disabledBackground}`
+    },
   }
 }))
 
