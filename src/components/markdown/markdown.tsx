dbef81a023a7ec263c9747233e469fd1149068af
Sarek <saregoth@yahoo.com>
2020-04-27 15:17:46 +0200

    Created markdown component

---
+++ src/components/markdown/markdown.tsx
@@ -0,0 +1,24 @@
+import SEO from "@components/seo"
+import { Container } from "@material-ui/core"
+import React from "react"
+import { MarkdownDoc } from "@utils/types/markdown"
+import useStyles from "./markdown.styles"
+
+const Markdown = ({data,title}: MarkdownDoc) => {
+  const classes = useStyles()
+  const { markdownRemark: {html} } = data 
+
+  return (
+    <>
+      <SEO title={title} />
+      <Container className={classes.root}>
+        <div
+         className={classes.markdown}
+         dangerouslySetInnerHTML={{ __html: html }}
+         />
+      </Container>
+    </>
+  )
+}
+
+export default Markdown
