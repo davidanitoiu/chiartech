a7faebf0b20983545205d5ffa8b0bc12f2a5164c
Sarek <saregoth@yahoo.com>
2020-04-29 20:48:36 +0200

    Moved pipeline components to project folder

--- src/assets/projects/interactive-flowchart/components/InteractiveFlowchart.tsx
+++ src/assets/projects/interactive-flowchart/components/InteractiveFlowchart.tsx
@@ -1,5 +1,5 @@
+import PipelineNodeInner from "@assets/projects/interactive-flowchart/components/PipelineNodeInner"
 import { chartSimple, validateLink } from "@assets/projects/interactive-flowchart/utils"
-import PipelineNodeInner from "@components/pipeline/PipelineNodeInner"
 import { actions, FlowChart } from "@mrblenny/react-flow-chart"
 import { cloneDeep, mapValues } from "lodash"
 import React, { useState } from "react"
@@ -7,19 +7,20 @@ import React, { useState } from "react"
 const InteractiveFlowchart = () => {
   const [chart] = useState(cloneDeep(chartSimple))
 
-  const stateActions = mapValues(actions, (func: any) => (...args: any) =>
-    console.log("magic")
+  const callbacks = mapValues(actions, (func: any) => (...args: any) =>
+    console.log("magic is happening")
   ) as typeof actions
 
   return (
     <>
       <FlowChart
         chart={chart}
-        callbacks={stateActions}
+        callbacks={callbacks}
         Components={{
           NodeInner: PipelineNodeInner,
         }}
         config={{
+          snapToGrid: true,
           validateLink,
         }}
       />
