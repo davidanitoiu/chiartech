c60790db96e327e590cfba6923ef721dc2ff9a86
Sarek <saregoth@yahoo.com>
2020-04-29 20:46:04 +0200

    Converted flowchart to function component

--- src/assets/projects/interactive-flowchart/components/InteractiveFlowchart.tsx
+++ src/assets/projects/interactive-flowchart/components/InteractiveFlowchart.tsx
@@ -1,33 +1,30 @@
-import React, { Component } from "react"
-import { actions, FlowChart } from "@mrblenny/react-flow-chart"
-import PipelineNodeInner from "@components/pipeline/PipelineNodeInner"
 import { chartSimple, validateLink } from "@assets/projects/interactive-flowchart/utils"
+import PipelineNodeInner from "@components/pipeline/PipelineNodeInner"
+import { actions, FlowChart } from "@mrblenny/react-flow-chart"
 import { cloneDeep, mapValues } from "lodash"
+import React, { useState } from "react"
 
-class InteractiveFlowchart extends Component {
-  state = cloneDeep(chartSimple)
+const InteractiveFlowchart = () => {
+  const [chart] = useState(cloneDeep(chartSimple))
 
-  render() {
-    const chart = this.state
-    const stateActions = mapValues(actions, (func: any) => (...args: any) =>
-      this.setState(func(...args))
-    ) as typeof actions
+  const stateActions = mapValues(actions, (func: any) => (...args: any) =>
+    console.log("magic")
+  ) as typeof actions
 
-    return (
-      <>
-        <FlowChart
-          chart={chart}
-          callbacks={stateActions}
-          Components={{
-            NodeInner: PipelineNodeInner,
-          }}
-          config={{
-            validateLink,
-          }}
-        />
-      </>
-    )
-  }
+  return (
+    <>
+      <FlowChart
+        chart={chart}
+        callbacks={stateActions}
+        Components={{
+          NodeInner: PipelineNodeInner,
+        }}
+        config={{
+          validateLink,
+        }}
+      />
+    </>
+  )
 }
 
 export default InteractiveFlowchart
