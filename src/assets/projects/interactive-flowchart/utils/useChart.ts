158e0b7fc7279e1788d558225004cb8056f035ff
Sarek <saregoth@yahoo.com>
2020-05-01 17:25:57 +0200

    Added prepare action to executeChartAction

--- src/assets/projects/interactive-flowchart/utils/useChart.ts
+++ src/assets/projects/interactive-flowchart/utils/useChart.ts
@@ -1,20 +1,24 @@
-import { actions, IChart, IFlowChartProps, IFlowChartCallbacks } from "@mrblenny/react-flow-chart";
-import { createAction, createReducer, AnyAction, PayloadAction } from "@reduxjs/toolkit";
+import { actions, IChart, IFlowChartCallbacks } from "@mrblenny/react-flow-chart";
+import { AnyAction, createAction, createReducer } from "@reduxjs/toolkit";
 import { mapValues } from "lodash";
-import { useReducer, Reducer } from "react";
+import { Reducer, useReducer } from "react";
 import { chartBase } from "./pipelineChart";
 
-export const useChart = (chart = chartBase):[IChart, IFlowChartCallbacks] => {
+export const useChart = (chart = chartBase): [IChart, IFlowChartCallbacks] => {
   const initialState = chart;
 
-  const executeChartAction = createAction('executeChartAction', );
+  const executeChartAction = createAction('executeChartAction', ({ action }) => ({
+    payload: {
+      action
+    }
+  }));
   const reducer = createReducer(initialState, {
     [executeChartAction.type]: (state, { payload }) => payload.action(state),
   })
 
-  const [state, dispatch] = useReducer<Reducer<IChart,AnyAction>>(reducer, initialState);
+  const [state, dispatch] = useReducer<Reducer<IChart, AnyAction>>(reducer, initialState);
 
-  const callbacks:IFlowChartCallbacks = mapValues(actions, (action: any) => (...args: any) =>
+  const callbacks: IFlowChartCallbacks = mapValues(actions, (action: any) => (...args: any) =>
     dispatch(executeChartAction({ action: action(...args) }))
   ) as typeof actions
 
