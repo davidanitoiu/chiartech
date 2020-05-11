0bda0d5edcf32c058642db833e8171079dca2730
Sarek <saregoth@yahoo.com>
2020-05-11 19:03:02 +0200

    playground.styles not valid in pages

--- src/pages/playground/index.tsx
+++ src/pages/playground/index.tsx
@@ -4,8 +4,11 @@ import { has } from "lodash"
 import React from "react"
 import { FaPlayCircle } from "react-icons/fa"
 import { GoMarkGithub } from "react-icons/go"
-import { useStyles } from "./playground.styles"
 import projects from "@utils/projects.json"
+import Color from "color"
+import svgBorder from '@images/border.svg';
+import { makeStyles, Theme } from "@material-ui/core"
+
 
 interface GithubProject {
   title: string
@@ -28,6 +31,59 @@ const githubProjects: Array<GithubProject> = projects.map(project => ({
   language: project.language,
   github: getGithubUrl(project.projectId),
   demo: getCodeSandboxUrl(project.projectId)
+}))
+
+const useStyles = makeStyles<Theme>(theme => ({
+  root: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100%",
+  },
+  projects: {
+      /* [theme.breakpoints.up("md")]: {
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr",
+          columnGap: theme.spacing(5),
+          rowGap: `${theme.spacing(5)}px`,
+      }, */
+      [theme.breakpoints.up("md")]: {
+          display: "flex",
+          height: "100%",
+          justifyContent: "space-evenly",
+          flexDirection: "column",
+      },
+      [theme.breakpoints.down("md")]: {
+          display: "flex",
+          height: "100%",
+          justifyContent: "space-evenly",
+          flexDirection: "column",
+      },
+  },
+  cardTitle: {
+      color: theme.palette.text.primary,
+  },
+  cardSubheader: {
+      color: theme.palette.text.secondary
+  },
+  card: {
+      background: `${Color(theme.palette.primary.main)
+          .darken(0.95)
+          .alpha(0.6)}`,
+          borderImage: `url(${svgBorder}) 50 50`,
+    boxSizing: 'border-box',
+    borderStyle: 'inset',
+    borderWidth: 20,
+      
+  },
+  cardContent: {
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      "& .Mui-disabled": {
+          color: theme.palette.text.primary
+      }
+  },
 }))
 
 const Playground = () => {
